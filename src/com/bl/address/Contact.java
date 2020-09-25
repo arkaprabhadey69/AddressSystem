8bf1a98dfaf5b21697a9e1aafe420933cd34bbc9
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-25 18:09:33 +0530

    [Arka]Added:Welcome Message

---
+++ src/com/bl/address/Contact.java
@@ -0,0 +1,38 @@
+package com.bl.address;
+import java.util.*;
+
+
+public class Contact {
+	
+	public String firstname;
+	public String lastname;
+	public String address;
+	public String city;
+	public String state;
+	public String zip;
+	public String number;
+	public String email;
+	public Contact(String firstname, String lastname, String address, String city, String state, String zip, String number,
+			String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.number = number;
+		this.email = email;
+	}
+	public Contact()
+	{
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address book");
+	}
+	
+	
+	
+	
+
+}
